3bed54e9c58b97db38276f1ed8fab40321b2572e
ersin-ertan <ersin_the_ertan@hotmail.com>
2015-08-08 17:25:44 -0400

    added code for card flip but have yet to explore the code, more graphics programming, will hold off. updated readme to have todo videos and will look at transitions and window animations for more general purpose skills

---
+++ app/src/main/java/com/nullcognition/animationtransitionmaterial/cardflip/CardFlipListener.java
@@ -0,0 +1,7 @@
+package com.nullcognition.animationtransitionmaterial.cardflip;
+// ersin 08/08/15 Copyright (c) 2015+ All rights reserved.
+
+public interface CardFlipListener{
+	void onCardFlipEnd();
+	void onCardFlipStart();
+}
